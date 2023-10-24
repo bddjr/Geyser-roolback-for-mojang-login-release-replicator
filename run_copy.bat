98b5d3f4bbe052b0d17960e5b0f5a4de4b068269
bddjr <ygyh5019@outlook.com>
2023-10-25 07:26:19 +0800

    first commit

---
+++ run_copy.bat
@@ -0,0 +1,3 @@
+@echo off
+python run_copy.py
+pause
---
+++ run_copy.py
@@ -0,0 +1,46 @@
+dist_dir = "./dist/"
+
+import shutil, os
+
+def in_path(name):
+    return os.path.abspath(
+        "../Geyser-roolback-for-mojang-login/bootstrap/" +
+        str.lower(name) +
+        "/build/libs/Geyser-" +
+        name +
+        ".jar"
+    )
+
+def out_path(name, dist_dir = dist_dir):
+    return os.path.abspath(
+        dist_dir +
+        "Geyser-roolback-for-mojang-login-" +
+        name +
+        ".jar"
+    )
+
+def cp(name):
+    shutil.copy(
+        in_path(name) ,
+        out_path(name)
+    )
+
+if os.path.exists(dist_dir):
+    shutil.rmtree(dist_dir)
+os.mkdir(dist_dir)
+
+
+cp("BungeeCord")
+cp("Fabric")
+cp("Spigot")
+cp("Standalone")
+cp("Velocity")
+
+'''
+shutil.copy(
+    in_path("Standalone") ,
+    out_path("Standalone", "./test-Standalone/")
+)
+'''
+
+print("OK!")
